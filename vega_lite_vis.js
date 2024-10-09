68cd20c3bf81ff505f4dbac4f5df3a6cd8d571e3
Chung Min Yi <you@example.com>
2024-10-09 22:58:33 +0800

    done

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,7 @@
 var vg_1 = 'Json/Map.json';
 var vg_2 = "Json/line.json";
 var vg_3 = "Json/scatter.json";
+var vg_4 = "Json/heatmap.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
   
@@ -10,5 +11,8 @@ vegaEmbed("#line", vg_2).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#scatter", vg_3).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#heatmap", vg_4).then(function(result) {
 }).catch(console.error);
 
