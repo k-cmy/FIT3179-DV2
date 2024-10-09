4aea716c129f9e4f2ceb7edf014bdb31012f65a0
Chung Min Yi <you@example.com>
2024-10-09 22:30:58 +0800

    update all change

---
+++ vega_lite_vis.js
@@ -0,0 +1,14 @@
+var vg_1 = 'Json/Map.json';
+var vg_2 = "Json/line.json";
+var vg_3 = "Json/scatter.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+  
+}).catch(console.error);
+
+vegaEmbed("#line", vg_2).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#scatter", vg_3).then(function(result) {
+}).catch(console.error);
+
