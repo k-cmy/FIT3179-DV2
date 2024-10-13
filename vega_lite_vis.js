8b5fffaad0b0521e2db415d6b7b79811a13b9a86
Chung Min Yi <you@example.com>
2024-10-14 00:50:58 +0800

    added area diagram

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,9 @@
 var vg_1 = 'Json/Map.json';
 var vg_2 = "Json/line.json";
 var vg_3 = "Json/scatter.json";
-var vg_4 = "Json/heatmap.json";
+var vg_4 = "Json/Area.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
-  
 }).catch(console.error);
 
 vegaEmbed("#line", vg_2).then(function(result) {
@@ -13,6 +12,5 @@ vegaEmbed("#line", vg_2).then(function(result) {
 vegaEmbed("#scatter", vg_3).then(function(result) {
 }).catch(console.error);
 
-vegaEmbed("#heatmap", vg_4).then(function(result) {
+vegaEmbed("#area", vg_4).then(function(result) {
 }).catch(console.error);
-
