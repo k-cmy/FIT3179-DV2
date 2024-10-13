4b3924418ac59724cf4ebff0ef922929b226303b
Chung Min Yi <you@example.com>
2024-10-14 06:44:09 +0800

    update new vis

--- vega_lite.js
+++ vega_lite.js
@@ -2,6 +2,7 @@ var vg_1 = 'Json/Map.json';
 var vg_2 = "Json/line.json";
 var vg_3 = "Json/line2.json";
 var vg_4 = "Json/Area.json";
+var vg_5 = "Json/streamgraph.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
 }).catch(console.error);
@@ -13,4 +14,7 @@ vegaEmbed("#line2", vg_3).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#area", vg_4).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#streamgraph", vg_5).then(function(result) {
 }).catch(console.error);
