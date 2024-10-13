94dca04aa315ff52cce3d994b9f3037a8184a594
Chung Min Yi <you@example.com>
2024-10-14 03:59:14 +0800

    update the visualization

--- vega_lite.js
+++ vega_lite.js
@@ -1,11 +1,15 @@
 var vg_1 = 'Json/Map.json';
 var vg_2 = "Json/line.json";
+var vg_3 = "Json/line2.json";
 var vg_4 = "Json/Area.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#line", vg_2).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#line2", vg_3).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#area", vg_4).then(function(result) {
