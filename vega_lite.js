f152d0b81e9e30580e56b520148edc9798bc6bf2
Chung Min Yi <you@example.com>
2024-10-14 01:08:29 +0800

    remove scatter

--- vega_lite.js
+++ vega_lite.js
@@ -1,15 +1,11 @@
 var vg_1 = 'Json/Map.json';
 var vg_2 = "Json/line.json";
-var vg_3 = "Json/scatter.json";
 var vg_4 = "Json/Area.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#line", vg_2).then(function(result) {
-}).catch(console.error);
-
-vegaEmbed("#scatter", vg_3).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#area", vg_4).then(function(result) {
