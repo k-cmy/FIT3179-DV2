b4ab7e96b6fe210cdda3f3bfe711692e85aad3ff
Chung Min Yi <you@example.com>
2024-10-14 20:46:59 +0800

    refactor

--- vega_lite.js
+++ vega_lite.js
@@ -1,20 +1,16 @@
 var vg_1 = 'Json/Map.json';
-var vg_2 = "Json/line.json";
-var vg_3 = "Json/line2.json";
+var vg_6 = "Json/combineline.json";
 var vg_4 = "Json/Area.json";
 var vg_5 = "Json/streamgraph.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
 }).catch(console.error);
 
-vegaEmbed("#line", vg_2).then(function(result) {
-}).catch(console.error);
-
-vegaEmbed("#line2", vg_3).then(function(result) {
-}).catch(console.error);
-
 vegaEmbed("#area", vg_4).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#streamgraph", vg_5).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#combineline", vg_6).then(function(result) {
 }).catch(console.error);
