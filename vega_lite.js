2ed998f7424f6e0167ea6f9ac9b7c182ad3d59d7
Chung Min Yi <you@example.com>
2024-10-15 21:20:09 +0800

    added checkboxes

--- vega_lite.js
+++ vega_lite.js
@@ -1,6 +1,7 @@
 var vg_1 = 'Json/Map.json';
 var vg_6 = "Json/combineline.json";
 var vg_4 = "Json/Area.json";
+var vg_2 = "Json/area2.json";
 var vg_5 = "Json/streamgraph.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
@@ -8,6 +9,9 @@ vegaEmbed("#map", vg_1).then(function(result) {
 
 vegaEmbed("#area", vg_4).then(function(result) {
 }).catch(console.error);
+
+vegaEmbed("#area2", vg_2).then(function(result) {
+}).catch(console.error);   
 
 vegaEmbed("#streamgraph", vg_5).then(function(result) {
 }).catch(console.error);
